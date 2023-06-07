af3123ad33a6d341fe8077a602250049cf5fea09
js-ha <jshaha0911@gmail.com>
2023-06-07 18:02:32 +0900

    🎨Design : 폰트 설정

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,11 @@
 import './globals.css';
+import { Do_Hyeon } from 'next/font/google';
+
+const do_Hyeon = Do_Hyeon({
+  subsets: ['latin'],
+  display: 'swap',
+  weight: '400',
+});
 
 export const metadata = {
   title: '글길 - 함께 쓰는 리뷰, 이야기가 되어 모이다.',
@@ -7,7 +14,7 @@ export const metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html>
-      <body>{children}</body>
+      <body className={do_Hyeon.className}>{children}</body>
     </html>
   );
 }
