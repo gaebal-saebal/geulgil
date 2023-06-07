3c5d6f5926d5eaa7d945c4e74ab98d795c39e79d
js-ha <jshaha0911@gmail.com>
2023-06-07 17:23:12 +0900

    파비콘 적용 테스트

--- app/page.tsx
+++ app/page.tsx
@@ -1,30 +1,3 @@
-'use client';
-
-import { useState } from 'react';
-
 export default function Home() {
-  const [lists, setLists] = useState([]);
-  const handleClick = () => {
-    fetch('api/get')
-      .then((res) => {
-        return res.json();
-      })
-      .then((data) => {
-        setLists(data.item);
-      });
-  };
-  console.log(lists);
-  return (
-    <main>
-      <button onClick={handleClick}>API통신보내기</button>
-
-      {lists.length > 0 ? (
-        lists.map((list: { title: string }, i) => {
-          return <div key={i}>{list.title}</div>;
-        })
-      ) : (
-        <div>로딩중입니다</div>
-      )}
-    </main>
-  );
+  return <div>메인페이지</div>;
 }
