fe0c62a2a1ce0d3f0ae27819aa2c934c61421936
js-ha <jshaha0911@gmail.com>
2023-06-07 17:07:45 +0900

    테스트 완료

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,30 @@
 'use client';
+
+import { useState } from 'react';
+
 export default function Home() {
+  const [lists, setLists] = useState([]);
   const handleClick = () => {
-    fetch('api/get', { method: 'GET' })
+    fetch('api/get')
       .then((res) => {
         return res.json();
       })
       .then((data) => {
-        console.log(data);
+        setLists(data.item);
       });
   };
-
+  console.log(lists);
   return (
     <main>
-      <button onClick={handleClick}>API통신보내기</button>메인페이지
+      <button onClick={handleClick}>API통신보내기</button>
+
+      {lists.length > 0 ? (
+        lists.map((list: { title: string }, i) => {
+          return <div key={i}>{list.title}</div>;
+        })
+      ) : (
+        <div>로딩중입니다</div>
+      )}
     </main>
   );
 }
--- pages/api/get.ts
+++ pages/api/get.ts
@@ -1,13 +1,15 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 
-export default function getAPI(res: NextApiResponse, req: NextApiRequest) {
+export default function getAPI(req: NextApiRequest, res: NextApiResponse) {
   if (req.method === 'GET') {
     fetch(
-      `http://book.interpark.com/api/bestSeller.api?key=${process.env.NEXT_PUBLIC_API_KEY}&categoryId=100`
+      `http://book.interpark.com/api/bestSeller.api?key=${process.env.NEXT_PUBLIC_API_KEY}&categoryId=100&output=json`
     )
       .then((res) => res.json())
-      .then((data) => console.log(data))
+      .then((data) => {
+        res.status(200).json(data);
+      })
+
       .catch(() => console.log('안됨'));
-    return res.status(200).json('잘됨');
   }
 }
