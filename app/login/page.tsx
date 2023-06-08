53cbcb8ebb8880255c1c3b6580ace9e51a72950e
js-ha <jshaha0911@gmail.com>
2023-06-08 17:49:47 +0900

    ✨Feat : 로그인 기능 구현

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,7 +1,14 @@
+'use client';
+
 import React from 'react';
+import { signIn } from 'next-auth/react';
 
 const Login = () => {
-  return <div>로그인페이지</div>;
+  return (
+    <div>
+      <button onClick={() => signIn()}>로그인</button>
+    </div>
+  );
 };
 
 export default Login;
---
+++ pages/api/auth/[...nextauth].ts
@@ -0,0 +1,73 @@
+// @ts-nocheck
+
+import { MongoDBAdapter } from '@next-auth/mongodb-adapter';
+import { connectDB } from '@/util/database';
+import NextAuth from 'next-auth';
+import GithubProvider from 'next-auth/providers/github';
+import CredentialsProvider from 'next-auth/providers/credentials';
+import bcrypt from 'bcrypt';
+
+export const authOptions = {
+  providers: [
+    GithubProvider({
+      clientId: process.env.NEXT_PUBLIC_GITHUB_CLIENTID,
+      clientSecret: process.env.NEXT_PUBLIC_GITHUB_CLIENTSECRET,
+    }),
+    CredentialsProvider({
+      //1. 로그인페이지 폼 자동생성해주는 코드
+      name: 'credentials',
+      credentials: {
+        // 로그인 페이지에 들어갈 인풋들 아래처럼 입력해주면 됨
+        email: { label: 'email', type: 'text' },
+        password: { label: 'password', type: 'password' },
+      },
+
+      //2. 로그인요청시 실행되는코드
+      //직접 DB에서 아이디,비번 비교하고
+      //아이디,비번 맞으면 return 결과, 틀리면 return null 해야함
+      async authorize(credentials) {
+        let db = (await connectDB).db('forum');
+        let user = await db.collection('user_cred').findOne({ email: credentials.email });
+        if (!user) {
+          console.log('해당 이메일은 없음');
+          return null;
+        }
+        const pwcheck = await bcrypt.compare(credentials.password, user.password);
+        if (!pwcheck) {
+          console.log('비번틀림');
+          return null;
+        }
+        return user;
+      },
+    }),
+  ],
+
+  //3. jwt 써놔야 잘됩니다 + jwt 만료일설정
+  session: {
+    strategy: 'jwt',
+    maxAge: 30 * 24 * 60 * 60, //30일 => 로그인 상태 유지기간
+  },
+
+  callbacks: {
+    //4. jwt 만들 때 실행되는 코드
+    //user변수는 DB의 유저정보담겨있고 token.user에 뭐 저장하면 jwt에 들어갑니다.
+    jwt: async ({ token, user }) => {
+      if (user) {
+        token.user = {}; //JWT에 기입할 정보
+        token.user.name = user.name;
+        token.user.email = user.email; // 로그아웃 해보고 다시 로그인 해야 추가한 정보 나옴
+        token.user.role = user.role;
+      }
+      return token;
+    },
+    //5. 유저 세션이 조회될 때 마다 실행되는 코드
+    session: async ({ session, token }) => {
+      session.user = token.user;
+      return session;
+    },
+  },
+
+  secret: process.env.NEXT_PUBLIC_PASSWORD,
+  adapter: MongoDBAdapter(connectDB, { databaseName: 'auth' }),
+};
+export default NextAuth(authOptions);
