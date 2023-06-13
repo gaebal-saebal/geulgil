dccdd00fdc05edd80836d1e8cff2dee60ee2b903
rosenfence <rosenfence@gmail.com>
2023-06-13 15:27:42 +0900

    🍎Chore : node-fetch 라이브러리 설치 및 적용

--- pages/api/get.ts
+++ pages/api/get.ts
@@ -1,15 +1,15 @@
 import { NextApiRequest, NextApiResponse } from 'next';
+import fetch from 'node-fetch';
 
 export default function getAPI(req: NextApiRequest, res: NextApiResponse) {
   if (req.method === 'GET') {
     fetch(
       `http://book.interpark.com/api/bestSeller.api?key=${process.env.NEXT_PUBLIC_API_KEY}&categoryId=100&output=json`
     )
-      .then((res) => res.json())
+      .then((response) => response.json())
       .then((data) => {
-        res.status(200).json(data);
+        return res.status(200).json(data);
       })
-
-      .catch(() => console.log('안됨'));
+      .catch(() => res.status(400).json('Bad Request'));
   }
 }
