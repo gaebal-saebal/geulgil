3f297f0bafba63eade365e19487248d87c6373d6
js-ha <jshaha0911@gmail.com>
2023-06-13 18:31:05 +0900

    ✨Feat : 리뷰 작성 기능 및 데이터베이스 저장 구현

--- app/book/[id]/page.tsx
+++ app/book/[id]/page.tsx
@@ -4,6 +4,8 @@ import { BookDetailType } from '@/types/interface';
 
 const Book = (props: { params: { id: string }; searchParams: {} }) => {
   const [lists, setLists] = useState<BookDetailType[]>([]);
+  const [content, setContent] = useState('');
+  const [rate, setRate] = useState('1');
   const id = props.params.id;
 
   useEffect(() => {
@@ -17,14 +19,48 @@ const Book = (props: { params: { id: string }; searchParams: {} }) => {
   if (lists.length > 0) {
     return (
       <div>
-        <div>{lists[0].categoryName}</div>
-        <img src={lists[0].coverLargeUrl} alt='book-cover' />
-        <h1>{lists[0].title}</h1>
-        <span>{lists[0].author}</span>
-        <span>{lists[0].publisher}</span>
-        <span>{lists[0].pubDate}</span>
-        <div>정가 : {lists[0].priceStandard}원</div>
-        <div>{lists[0].description}</div>
+        <div>
+          <div>{lists[0].categoryName}</div>
+          <img src={lists[0].coverLargeUrl} alt='book-cover' />
+          <h1>{lists[0].title}</h1>
+          <span>{lists[0].author}</span>
+          <span>{lists[0].publisher}</span>
+          <span>{lists[0].pubDate}</span>
+          <div>정가 : {lists[0].priceStandard}원</div>
+          <div>{lists[0].description}</div>
+        </div>
+        <div>
+          <select
+            name='별점'
+            onChange={(e) => {
+              setRate(e.target.value);
+            }}
+          >
+            <option value='1'>1점</option>
+            <option value='2'>2점</option>
+            <option value='3'>3점</option>
+            <option value='4'>4점</option>
+            <option value='5'>5점</option>
+          </select>
+          <textarea
+            onChange={(e) => {
+              setContent(e.target.value);
+            }}
+          />
+          <div>좋아요</div>
+          <button
+            onClick={() => {
+              fetch('/api/reviews/createReview', {
+                method: 'POST',
+                body: JSON.stringify({ rate, content }),
+              })
+                .then((res) => res.json())
+                .then((data) => console.log(data));
+            }}
+          >
+            리뷰 올리기
+          </button>
+        </div>
       </div>
     );
   } else {
---
+++ pages/api/reviews/createReview.ts
@@ -0,0 +1,36 @@
+import { connectDB } from '@/util/database';
+import { NextApiRequest, NextApiResponse } from 'next';
+import { getServerSession } from 'next-auth';
+import { authOptions } from '../auth/[...nextauth]';
+import { SessionType, ReviewDataType } from '@/types/interface';
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  let request = JSON.parse(req.body);
+
+  let session: SessionType | null = await getServerSession(req, res, authOptions);
+
+  if (req.method === 'POST') {
+    if (session) {
+      let date = new Date();
+      let year = date.getFullYear();
+      let month = ('0' + (date.getMonth() + 1)).slice(-2);
+      let day = ('0' + date.getDate()).slice(-2);
+      let dateResult = `${year}-${month}-${day}`;
+      let reviewData: ReviewDataType = {
+        rate: request.rate,
+        content: request.content,
+        date: dateResult,
+        likes: 0,
+      };
+
+      reviewData.name = session.user.name;
+
+      const db = (await connectDB).db('book');
+      const write = await db.collection('review').insertOne(reviewData);
+      return res.status(200).json('리뷰데이터 저장완료');
+    } else {
+      res.status(400).json('로그인해주세요');
+    }
+  }
+  res.status(400).json('잘못된 요청입니다.');
+}
