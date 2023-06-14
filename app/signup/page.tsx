01c6264ac1580935eb4fd90f014ffa68224bff82
rosenfence <rosenfence@gmail.com>
2023-06-14 18:07:37 +0900

    ✨Feat : 회원가입 다른 page/component 통일성을 위해 fetch로 변경

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -1,14 +1,59 @@
-import React from 'react';
+'use client';
+import React, { useState } from 'react';
 
 const Signup = () => {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>, type: string): void => {
+    if (type === 'name') {
+      setName(e.target.value);
+    } else if (type === 'email') {
+      setEmail(e.target.value);
+    } else if (type === 'password') {
+      setPassword(e.target.value);
+    }
+  };
+
+  const handleSubmit = () => {
+    fetch('/api/auth/signUp', {
+      method: 'POST',
+      body: JSON.stringify({ name, email, password }),
+    })
+      .then((res) => {
+        res.json();
+      })
+      .then((data) => console.log(data))
+      .catch((err) => console.log(err));
+  };
+
   return (
     <div>
-      <form method='POST' action='/api/auth/signUp'>
-        <input name='name' type='text' placeholder='이름' />
-        <input name='email' type='text' placeholder='이메일' />
-        <input name='password' type='password' placeholder='비번' />
-        <button type='submit'>id/pw 가입요청</button>
-      </form>
+      <div>
+        <input
+          onChange={(e) => {
+            handleChange(e, 'name');
+          }}
+          type='text'
+          placeholder='이름'
+        />
+        <input
+          onChange={(e) => {
+            handleChange(e, 'email');
+          }}
+          type='text'
+          placeholder='이메일'
+        />
+        <input
+          onChange={(e) => {
+            handleChange(e, 'password');
+          }}
+          type='password'
+          placeholder='비번'
+        />
+        <button onClick={handleSubmit}>id/pw 가입요청</button>
+      </div>
     </div>
   );
 };
