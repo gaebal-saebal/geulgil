620b2b137767adcd8322d3435856ea35e9f9ee20
rosenfence <rosenfence@gmail.com>
2023-06-14 16:45:09 +0900

    ✨Feat : 현재 로그인 세션 받아오는 기능 구현

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import NavBar from '@/components/NavBar';
 import './globals.css';
 import { Do_Hyeon } from 'next/font/google';
+import { getServerSession } from 'next-auth';
+import { authOptions } from '@/pages/api/auth/[...nextauth]';
 
 const do_Hyeon = Do_Hyeon({
   subsets: ['latin'],
@@ -12,11 +14,13 @@ export const metadata = {
   title: '글길 - 함께 쓰는 리뷰, 이야기가 되어 모이다.',
 };
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default async function RootLayout({ children }: { children: React.ReactNode }) {
+  const session = await getServerSession(authOptions);
+
   return (
     <html>
       <body className={do_Hyeon.className}>
-        <NavBar />
+        <NavBar session={session} />
         {children}
       </body>
     </html>
