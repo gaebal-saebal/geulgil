a1ce9f55e9c8dc74fdc1ae0b5a11a53c481b0a00
rosenfence <rosenfence@gmail.com>
2023-06-14 16:47:01 +0900

    🍎Chore : 메인페이지 버튼 삭제

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,10 @@
 'use client';
 import React from 'react';
-import Button from '@/components/Button';
 import BookListOnMain from '@/components/BookListOnMain';
 
-export default function Home() {
+export default async function Home() {
   return (
     <div>
-      <Button
-        onClick={() => {
-          fetch('/api/new', { method: 'POST' })
-            .then((res) => res.json())
-            .then((data) => console.log(data));
-        }}
-      >
-        글쓰기
-      </Button>
       <BookListOnMain />
     </div>
   );
