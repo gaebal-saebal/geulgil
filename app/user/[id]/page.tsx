88a66b728a7cc45dcbff74f0ac8d0f643d2bd905
js-ha <jshaha0911@gmail.com>
2023-06-15 18:22:35 +0900

    ✨Feat :  유저페이지 읽은책, 작성한 리뷰 렌더링 구현

--- app/user/[id]/page.tsx
+++ app/user/[id]/page.tsx
@@ -1,7 +1,56 @@
-import React from 'react';
+'use client';
 
-const User = () => {
-  return <div>유저페이지</div>;
+import Link from 'next/link';
+import React, { useEffect, useState } from 'react';
+
+const User = (props: { params: { id: string }; searchParams: {} }) => {
+  let userId = props.params.id;
+  const [myReviews, setMyReviews] = useState<{ content: string; isbn: string }[]>([]);
+  const [myReviewImgs, setMyReviewImgs] = useState<{ img: string; isbn: string }[]>([]);
+  // 읽은책리스트
+  // 작성한리뷰
+  // 리뷰 작성하나당 1점이런식으로 점수도?
+  // 회원정보수정
+  // 회원탈퇴
+
+  useEffect(() => {
+    getMyReviewInfo();
+  }, []);
+
+  const getMyReviewInfo = () => {
+    fetch(`/api/users/getUserInfo?userId=${userId}`)
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        setMyReviewImgs(data.myReviewImg);
+        setMyReviews(data.userReviews);
+      })
+      .catch((err) => console.log(err));
+  };
+
+  console.log(myReviews);
+  console.log(myReviewImgs);
+  return (
+    <>
+      <div>읽은 책 이미지</div>
+      {myReviewImgs.map((img, i) => {
+        return (
+          <Link key={i} href={`/book/${img.isbn}`}>
+            <img src={img.img} />
+          </Link>
+        );
+      })}
+      <div>작성한리뷰</div>
+      {myReviews.map((review, i) => {
+        return (
+          <Link key={i} href={`/book/${review.isbn}`}>
+            <div>{review.content}</div>
+          </Link>
+        );
+      })}
+    </>
+  );
 };
 
 export default User;
