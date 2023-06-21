bc630d161f15bced87955c6e40575ac9985eb8e8
js-ha <jshaha0911@gmail.com>
2023-06-21 18:22:08 +0900

    ✨Feat : 계정 삭제 확인 모달 생성

---
+++ components/ConfirmModal.tsx
@@ -0,0 +1,36 @@
+import { ModalProps } from '@/types/type';
+import React from 'react';
+
+const ConfirmModal = ({ onClick, onClose }: ModalProps) => {
+  return (
+    <div
+      onClick={onClose}
+      className='flex justify-center items-center absolute inset-0 z-50 bg-[rgba(0,0,0,0.5)] '
+    >
+      <form
+        onClick={(e) => e.stopPropagation()}
+        className='relative w-[300px] h-[200px] bg-white overflow-hidden rounded-md shadow-lg shadow-indigo-500/50 p-[20px] '
+      >
+        <div className='flex flex-col justify-center h-3/4 '>
+          <span>정말 탈퇴하시겠습니까? 이 동작은 취소할 수 없습니다.</span>
+        </div>
+        <div className='flex items-end h-1/4 justify-evenly'>
+          <button
+            onClick={onClick}
+            className='px-3 py-2 text-white bg-orange-300 rounded-md hover:bg-orange-500'
+          >
+            확인
+          </button>
+          <button
+            onClick={onClose}
+            className='px-3 py-2 text-white bg-orange-300 rounded-md hover:bg-orange-500'
+          >
+            취소
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default ConfirmModal;
