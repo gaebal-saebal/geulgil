7447193d1be4356bd82776ed514bb6a41201f277
rosenfence <rosenfence@gmail.com>
2023-06-22 11:33:07 +0900

    ✨Feat : 닉네임 수정 시 작성 리뷰에 적용된 닉네임도 수정되는 기능 구현

--- pages/api/users/patchUserName.ts
+++ pages/api/users/patchUserName.ts
@@ -5,34 +5,33 @@ import { getServerSession } from 'next-auth';
 import { authOptions } from '../auth/[...nextauth]';
 import { ObjectId } from 'mongodb';
 
-//TODO: 작성한 리뷰 찾아서 book-review의 name들을 바뀐 닉네임으로 변경
-
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   let db = (await connectDB).db('auth');
   let bookDb = (await connectDB).db('book');
   let session: SessionType | null = await getServerSession(req, res, authOptions);
   // session에 userId가 담겨져있음
 
-  /*
-  const userAllReview = await bookDb
-        .collection('review')
-        .find({ userId: 세션에담긴userId })
-        .toArray();
-
-        => 그 사람이 작성한 리뷰 다 가져올 수 있음
-
-        userAllReview에 반복문을 돌려서
-        userAllReview[0]~userAllReview[끝]의 name을 req.body(바꿀이름)으로 수정하고
-        updateOne을 사용해서 set에 담아주기만 하면 끝
-  */
-
   if (req.method === 'PATCH') {
     if (session) {
+      // auth - user의 닉네임 항목 수정
       const userInfo = await db.collection('users').findOne({ email: session.user.email });
       userInfo.name = req.body;
       const userNameChange = await db
         .collection('users')
         .updateOne({ _id: new ObjectId(userInfo._id) }, { $set: userInfo });
+
+      // 모든 리뷰에 적용된 닉네임도 바뀐 닉네임으로 수정
+      const userAllReviews = await bookDb
+        .collection('review')
+        //@ts-ignore
+        .find({ userId: session.user._id })
+        .toArray();
+      for (let i = 0; i < userAllReviews.length; i++) {
+        userAllReviews[i].name = req.body;
+        const updateUserNameInReview = await bookDb
+          .collection('review')
+          .updateOne({ _id: new ObjectId(userAllReviews[i]._id) }, { $set: userAllReviews[i] });
+      }
       return res.status(200).json(userInfo);
     }
     return res.status(400).json('bad request');
