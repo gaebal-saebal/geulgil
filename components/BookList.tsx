54d7725df44ad9da7a0372c9d3e5c52a09027ea8
js-ha <jshaha0911@gmail.com>
2023-06-23 13:35:45 +0900

    ✨Feat : 북리스트 스와이퍼 기능 구현

--- components/BookList.tsx
+++ components/BookList.tsx
@@ -1,23 +1,50 @@
 import { BookListOnMainType } from '@/types/interface';
 import Link from 'next/link';
 import React from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import { Navigation } from 'swiper';
 
 const BookList = ({ lists }: { lists: BookListOnMainType[] }) => {
   return (
-    <div className='max-w-screen-2xl'>
-      <ul className='grid grid-cols-2 gap-4 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5'>
+    <div className='max-w-screen-lg mb-24 bg-gray-100 pb-6 rounded'>
+      <Swiper
+        spaceBetween={30}
+        modules={[Navigation]}
+        navigation={true}
+        breakpoints={{
+          0: {
+            slidesPerView: 2,
+            slidesPerGroup: 2,
+          },
+          700: {
+            slidesPerView: 3,
+            slidesPerGroup: 3,
+          },
+          950: {
+            slidesPerView: 4,
+            slidesPerGroup: 4,
+          },
+          1280: {
+            slidesPerView: 5,
+            slidesPerGroup: 5,
+          },
+        }}
+        className=' px-16 '
+      >
         {lists.map((list, i) => {
           return (
-            <li key={i} className='flex flex-col p-6'>
-              <Link href={`/book/${list.isbn}`}>
+            <SwiperSlide key={i} className='flex flex-col items-center'>
+              <Link href={`/book/${list.isbn}`} className='h-[300px] flex items-center'>
                 <img src={list.coverLargeUrl} />
               </Link>
-              <div>{list.title}</div>
-              <div>{list.author}</div>
-            </li>
+              <div className='w-full truncate'>{list.title}</div>
+              <div className='w-full text-sm text-gray-500'>{list.author}</div>
+            </SwiperSlide>
           );
         })}
-      </ul>
+      </Swiper>
     </div>
   );
 };
