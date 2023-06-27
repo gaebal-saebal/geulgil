bc55aa409574f1f26f84e6692b410a24058805e5
rosenfence <rosenfence@gmail.com>
2023-06-27 18:25:22 +0900

    🔨Fix : Link prefetch={false} 추가

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -17,16 +17,17 @@ const Footer = () => {
     };
   }, []);
   return (
-    <div className='flex flex-col w-full absolute bottom-0 left-0 bg-black text-white h-64 p-6 justify-between items-center'>
-      <div className='max-w-screen-xl w-full h-full flex flex-col justify-between'>
+    <div className='absolute bottom-0 left-0 flex flex-col items-center justify-between w-full h-64 p-6 text-white bg-black'>
+      <div className='flex flex-col justify-between w-full h-full max-w-screen-xl'>
         <div className='flex justify-between'>
           <ul>
-            <div className='text-gray-300 mb-2'>DEVELOPER</div>
+            <div className='mb-2 text-gray-300'>DEVELOPER</div>
             <li className='mb-1'>
               <Link
                 href='https://github.com/rosenfence'
                 target='_blank'
                 className='hover:text-orange-400'
+                prefetch={false}
               >
                 신창하
               </Link>
@@ -36,6 +37,7 @@ const Footer = () => {
                 href='https://github.com/js-ha'
                 target='_blank'
                 className='hover:text-orange-400'
+                prefetch={false}
               >
                 하지수
               </Link>
@@ -46,6 +48,7 @@ const Footer = () => {
               className='mr-2 hover:text-orange-400'
               href='https://github.com/gaebal-saebal/geulgil'
               target='_blank'
+              prefetch={false}
             >
               <FaGithub />
             </Link>
@@ -53,26 +56,27 @@ const Footer = () => {
               className='mr-2 hover:text-orange-400'
               href='http://pf.kakao.com/_xexaxePxj/chat'
               target='_blank'
+              prefetch={false}
             >
               <RiKakaoTalkFill />
             </Link>
-            <Link href='/' className='hover:text-orange-400'>
+            <Link href='/' className='hover:text-orange-400' prefetch={false}>
               <FaInstagramSquare />
             </Link>
           </div>
         </div>
         <div className='flex w-full'>
           {screenWidth > 768 ? (
-            <div className=' w-1/5'>
+            <div className='w-1/5 '>
               <img src='/imgLogo.svg' alt='big-logo' className='h-16' />
             </div>
           ) : (
-            <div className=' w-1/5'>
+            <div className='w-1/5 '>
               <img src='/ico.png' alt='small-logo' className='h-14' />
             </div>
           )}
 
-          <div className='flex w-4/5 justify-between items-end'>
+          <div className='flex items-end justify-between w-4/5'>
             <div className='pl-3 text-xs'>
               © 2023 Gaebal-Saebal
               <Link href='https://github.com/gaebal-saebal/geulgil'>{` <https://github.com/gaebal-saebal>`}</Link>
@@ -81,14 +85,16 @@ const Footer = () => {
               <Link
                 href='http://pf.kakao.com/_xexaxePxj/chat'
                 target='_blank'
-                className='hover:text-orange-400 text-sm'
+                className='text-sm hover:text-orange-400'
+                prefetch={false}
               >
                 문의상담
               </Link>
               <Link
                 href='http://pf.kakao.com/_xexaxePxj/chat'
                 target='_blank'
-                className='hover:text-orange-400 text-sm'
+                className='text-sm hover:text-orange-400'
+                prefetch={false}
               >
                 광고문의
               </Link>
