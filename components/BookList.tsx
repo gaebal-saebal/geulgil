5e1ffd6678bce02650de96f803d6c671f9cae43d
js-ha <jshaha0911@gmail.com>
2023-06-27 13:42:33 +0900

    🔨Fix : 외국도서 검색 오류 해결

--- components/BookList.tsx
+++ components/BookList.tsx
@@ -7,6 +7,13 @@ import 'swiper/css/navigation';
 import { Navigation } from 'swiper';
 
 const BookList = ({ lists }: { lists: BookListOnMainType[] }) => {
+  const changeCategory = (name: string) => {
+    if (name === '외국도서') {
+      return 'foreign';
+    } else {
+      return 'book';
+    }
+  };
   return (
     <div className='w-full max-w-screen-xl pb-6 mb-24 bg-gray-100 rounded'>
       <Swiper
@@ -40,9 +47,12 @@ const BookList = ({ lists }: { lists: BookListOnMainType[] }) => {
         {lists.map((list, i) => {
           return (
             <SwiperSlide key={i} className='flex flex-col items-center'>
-              <Link href={`/book/${list.isbn}`} className='h-[300px] flex items-center'>
+              <Link
+                href={`/book/${list.isbn}?searchTarget=${changeCategory(list.categoryName)}`}
+                className='h-[300px] flex items-center'
+              >
                 <div className='relative'>
-                  <img src={list.coverLargeUrl} />
+                  <img src={list.coverLargeUrl} className='shadow-lg' />
                   <div className='absolute top-0 left-0 w-8 h-8 text-white bg-orange-500 border flex-center'>
                     {i + 1}
                   </div>
