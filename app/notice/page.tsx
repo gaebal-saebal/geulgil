be5159fd3a8378cbe34bdf6b1fa7c7416e57ce6c
js-ha <jshaha0911@gmail.com>
2023-06-28 12:05:28 +0900

    🎨Design : notice 반응형 디자인 수정

--- app/notice/page.tsx
+++ app/notice/page.tsx
@@ -4,7 +4,7 @@ import notice from './notice';
 
 const Notice = () => {
   return (
-    <div className='flex flex-col items-center mt-16'>
+    <div className='flex flex-col items-center mt-16 mb-16'>
       <div className='mb-10 text-4xl'>NOTICE</div>
       {notice.map((item, i) => {
         return <Accordion title={item.title} content={item.content} key={i} />;
