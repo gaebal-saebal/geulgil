c7ff6e1bec10521bbaeb07a65851e881ee3a0f3d
js-ha <jshaha0911@gmail.com>
2023-06-28 12:05:15 +0900

    🎨Design : faq 반응형 디자인 수정

--- app/faq/page.tsx
+++ app/faq/page.tsx
@@ -4,7 +4,7 @@ import faq from './faq';
 
 const Faq = () => {
   return (
-    <div className='flex flex-col items-center mt-16'>
+    <div className='flex flex-col items-center mt-16 mb-16'>
       <div className='mb-10 text-4xl'>FAQ</div>
       {faq.map((item, i) => {
         return <Accordion title={item.title} content={item.content} key={i} />;
