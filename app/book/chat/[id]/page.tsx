d798006f7c1d4578de113bae197317673bdec07d
rosenfence <rosenfence@gmail.com>
2023-07-10 12:25:57 +0900

    ✨Feat : 채팅 페이지 라우팅 구현

---
+++ app/book/chat/[id]/page.tsx
@@ -0,0 +1,18 @@
+'use client';
+import React from 'react';
+
+// TODO: 이전에
+// 로그인 안 되어 있으면 로그인하라고 로그인페이지로 보내버리기
+// 서버기능
+// book-review에서 [id] = isbn인 review들 중에서
+// userid = 현재 로그인 하고 있는 사람의 id 인 리뷰가 하나 이상 있으면 res를 200으로 보내고
+// 없으면 res 400으로 보낸다
+// 클라이언트기능
+// response가 200이면 채팅방에 접속시켜주고
+// response가 200이 아니면 빠꾸먹인다
+
+const Chat = () => {
+  return <div>채팅페이지에요</div>;
+};
+
+export default Chat;
